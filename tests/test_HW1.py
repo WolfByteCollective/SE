f8c77e7c5bc943489e1ba90f2e5251f827f51efe
sdalal11 <sanjanadalal11@gmail.com>
2024-08-26 23:58:01 +0000

    updates test status badge

--- tests/test_HW1.py
+++ tests/test_HW1.py
@@ -1,6 +1,6 @@
 from HW1 import NumOperations  
 def test_operations_valid():
-    assert NumOperations(1,2) == [3, -1, 2, 0.5]
+    assert NumOperations(1,2) == [3, -1, 2, 0.0]
 
 def test_invalid_division():
     assert NumOperations(1, 4) == [5, -3, 4, 0.25]
