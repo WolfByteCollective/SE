44a09e8f0b5ea92c70515839b0224be688c3d534
AkhilChawla <46402073+AkhilChawla@users.noreply.github.com>
2024-08-27 05:49:53 +0000

    updates test cases as expected

--- tests/test_HW1.py
+++ tests/test_HW1.py
@@ -1,8 +1,6 @@
 from HW1 import NumOperations  
 def test_operations_valid():
-    # assert NumOperations(1,2) == [3, -1, 2, 0.5]
-    assert 1==1
+    assert NumOperations(1,2) == [3, -1, 2, 0.5]
 
 def test_invalid_division():
-    assert 1==1
-    # assert NumOperations(1, 4) == [5, -3, 4, 0.2]
+    assert NumOperations(1, 4) == [5, -3, 4, 0.2]
