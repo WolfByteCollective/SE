bd39d8464fa601c085f1d94219305ad5155b55da
AkhilChawla <46402073+AkhilChawla@users.noreply.github.com>
2024-08-28 19:40:49 -0400

    Update test_HW1.py

--- tests/test_HW1.py
+++ tests/test_HW1.py
@@ -3,4 +3,4 @@ def test_operations_valid():
     assert NumOperations(1,2) == [3, -1, 2, 0.5]
 
 def test_invalid_division():
-    assert NumOperations(1, 4) == [5, -3, 4, 0.25]
+    assert NumOperations(1, 4) == [5, -3, 4, 0.2]
