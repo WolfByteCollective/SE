e46e08d3a3021c4c8524c413ea50cbf88c2c9c6c
sdalal11 <sanjanadalal11@gmail.com>
2024-09-08 18:08:07 +0000

    rand debugging using pylint

--- HW2/rand.py
+++ HW2/rand.py
@@ -2,9 +2,11 @@
 import subprocess
 
 def random_array(arr):
-    """Function random array"""
+    """Function of random array"""
     shuffled_num = None
     for i, in enumerate(arr):
         shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
+
+
