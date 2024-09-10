43e74636fd3fcf53db2a6c96152be112db3b8010
AkhilChawla <46402073+AkhilChawla@users.noreply.github.com>
2024-09-10 20:13:36 +0000

    fixed lint issues

--- HW2/rand.py
+++ HW2/rand.py
@@ -4,6 +4,6 @@ import secrets
 
 def random_array(arr):
     """Function to shuffle array with cryptographically secure random numbers"""
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         arr[i] = secrets.randbelow(20) + 1  # Generate secure random number between 1 and 20
     return arr
