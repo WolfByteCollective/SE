47c0e13384e896d7a5bf7139a495bf5b97240bb2
AkhilChawla <46402073+AkhilChawla@users.noreply.github.com>
2024-09-12 05:25:01 +0000

    fixed test case

--- HW2/tests/test_hw2.py
+++ HW2/tests/test_hw2.py
@@ -1,14 +1,14 @@
 'Test file for HW2 test cases'
 from hw2_debugging import merge_sort
 
-def test_sorting_valid_odd():
-    '''Test for successful sorting'''
+def test_sorting_valid_odd_length():
+    '''Test successful sorting for odd number of elements'''
     assert merge_sort([1,6,7,5,4,2,3]) == [1,2,3,4,5,6,7]
 
 def test_sorting_valid_even():
-    '''Test for successful sorting for even number of elements'''
+    '''Test successful sorting for even number of elements'''
     assert merge_sort([1,6,7,8,5,4,2,3]) == [1,2,3,4,5,6,7,8]
 
 def test_sorting_single_element():
-    '''Test for successful sorting'''
-    assert merge_sort([1]) == []
+    '''Test successful sorting for single element in the array'''
+    assert merge_sort([1]) == [1]
