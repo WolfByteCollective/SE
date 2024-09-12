c210dfbd7723256a84382b8049dc29e47b6d1238
AkhilChawla <46402073+AkhilChawla@users.noreply.github.com>
2024-09-12 05:44:20 +0000

    made code PEP 8 complaint

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,8 +1,10 @@
 '''This is HW1 for SE'''
-def num_operations(a,b):
+
+
+def num_operations(a, b):
     '''Function to get the sum, difference, product and division of numbers'''
     add = a + b
-    diff = a-b
-    product = a*b
-    div = a/b
+    diff = a - b
+    product = a * b
+    div = a / b
     return [add, diff, product, div]
--- HW2/tests/test_hw2.py
+++ HW2/tests/test_hw2.py
@@ -1,13 +1,16 @@
 'Test file for HW2 test cases'
 from hw2_debugging import merge_sort
 
+
 def test_sorting_valid_odd_length():
     '''Test successful sorting for odd number of elements'''
-    assert merge_sort([1,6,7,5,4,2,3]) == [1,2,3,4,5,6,7]
+    assert merge_sort([1, 6, 7, 5, 4, 2, 3]) == [1, 2, 3, 4, 5, 6, 7]
+
 
 def test_sorting_valid_even():
     '''Test successful sorting for even number of elements'''
-    assert merge_sort([1,6,7,8,5,4,2,3]) == [1,2,3,4,5,6,7,8]
+    assert merge_sort([1, 6, 7, 8, 5, 4, 2, 3]) == [1, 2, 3, 4, 5, 6, 7, 8]
+
 
 def test_sorting_single_element():
     '''Test successful sorting for single element in the array'''
